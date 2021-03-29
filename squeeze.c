fb4ebbe657a8304af8a0271f49852ee7b39c9e33
root <453351949@qq.com>
2021-03-29 05:17:49 +0000

    first time

---
+++ squeeze.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+void squeeze(char s1[], char s2[]);
+
+int main(){
+    char s1[] = "This is my answer";
+    char s2[] = "is";
+    squeeze(s1, s2);
+    printf("%s\n", s1);
+    return 0;
+}
+
+
+void squeeze(char s1[], char s2[])
+{
+    int i, j , k;
+
+    for (i = j = 0; s1[i] != '\0'; i++) {
+        for (k = 0; s2[k] != '\0'; k++) {
+            if (s1[i] == s2[k]){
+                break;
+            }
+        }
+        if (s2[k] == '\0'){
+            s1[j++] = s1[i];
+        }
+
+    }
+    s1[j] = '\0';
+}
+
+
